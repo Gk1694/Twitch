741df350cce19a56f5bb30eba3f0c6b0bda398d5
Ghanshyam Kandu <1694gk@gmail.com>
2021-11-03 20:47:12 +0530

    added the plan history router

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from './stream/StreamCreate';
 import StreamEdit from './stream/StreamEdit';
 import StreamDelete from './stream/StreamDelete';
 import StreamList from './stream/StreamList';
 import StreamShow from './stream/StreamShow';
 import Header from "./Header";
+import history from "../history";
 
 
 const App = () => {
     return (
     <div className="ui container" >
-        <BrowserRouter>
+        <Router history={history} >
             <div>
                 <Header />
                 <Route path="/" exact component={StreamList} />
                 <Route path="/stream/new" exact component={StreamCreate} />
-                <Route path="/stream/edit" exact component={StreamEdit} />
-                <Route path="/stream/delete" exact component={StreamDelete} />
+                <Route path="/stream/edit/:id" exact component={StreamEdit} />
+                <Route path="/stream/delete/:id" exact component={StreamDelete} />
                 <Route path="/stream/show" exact component={StreamShow} />
             </div>
-        </BrowserRouter>
+        </Router>
     </div>
     );
 };
